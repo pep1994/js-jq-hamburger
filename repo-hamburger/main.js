44548c8b27f4c5cb81db7189e1b6dac710627919
Marco Petrini <marcopetrini1994@gmail.com>
2020-03-31 14:47:55 +0200

    creazione di due funzioni che hanno il compito di far apparire e scomparire il menu

--- repo-hamburger/main.js
+++ repo-hamburger/main.js
@@ -9,22 +9,26 @@ se cliccko che è già visibile, invece scompare.
 var menuVisible = "hidden";
 
 
-// seleziono l'hamburger menu attraverso jquery e gli dico di mostrare il menu quando viene clikkato
-$('.fas.fa-bars').click(
-  function () {
-    if (menuVisible === "hidden") {
-      $('.hamburger-menu').show(1000);
-      menuVisible = "visible"
-    }
-  }
-);
+// seleziono l'hamburger menu attraverso jquery e gli dico di eseguire una funzione
+$('.fas.fa-bars').click(hamburgerMenuShow);
 
-// seleziono la "x" di chiusura menu e gli dico di chiudere il menu quando viene clikkata
-$('.close').click(
-  function() {
-    if (menuVisible === "visible") {
-      $('.hamburger-menu').hide("fast");
-      menuVisible = "hidden";
-    }
+// seleziono la "x" di chiusura menu e gli dico di eseguire una funzione
+$('.close').click(hamburgerMenuHide);
+
+
+// creo una funzione per la visibilità dell'hamburger-menu al click
+function hamburgerMenuShow () {
+  if (menuVisible === "hidden") {
+    $(".hamburger-menu").show(1000);
+    menuVisible = "visible";
   }
-);
+}
+
+
+// creo una funzione in modo che quando si clikka sulla "x" dell'hamburger-menu, quest'ultimo scompaia
+function hamburgerMenuHide () {
+  if (menuVisible !== "hidden") {
+    $(".hamburger-menu").hide("fast");
+    menuVisible = "hidden";
+  }
+}
